d4a1201f5ee53c7fb96a5bb97c8b2c75aa6cf0ef
TotallyFine <32402877+TotalyFine@users.noreply.github.com>
2025-03-03 19:49:42 +0000

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,31 @@
 import streamlit as st
 
-# #streamlit run streamlit_app.py
 
 def app():
-    st.set_page_config(
-        page_title="Home",
-        layout="wide",
-        initial_sidebar_state="expanded"
-    )
+    st.set_page_config(layout="wide")
+    pages = {
+        "Meniu": [
+            st.Page("streamlit_app.py", title="Pagrindinis"),
+            st.Page("./sections/products.py", title="Gaminiai"),
+            st.Page("./sections/Demo.py", title="Demo lentelės")
+        ]
+    }
+
+    pg = st.navigation(pages)
+    pg.run()
 
     st.write("""
-# Production planer demo
+# Gamybos planavimas: pavyzdys
+Čia yra pagrindinis puslapis. Galima atvaizduoti bet kokią informaciją lentelės, diagramos ar teksto pavidalu. Pvz.: paskutinės 5 pagamintų prekių arba paskutinės įtrauktos į gamybą.
+Kairėje yra menu. Galima kiekvieną funkciją išskaidyti į atskirus puslapius. Trumpai apie kiekvieną iš puslapių:
 
 """)
+    
+    st.write("### Gaminiai")
+    st.write("Visa informacija susijusi su gaminiais. Gaminių ir ingredientų sąrašas. Gaminių savikainos paskaičiuotos pagal nustatytas kainas kurios yra taip pat paimtos iš Excel failų")
 
 
 if __name__ == "__main__":
     app()
+
+
